9e2263c235b561a3e709cd08ecb4c006fae0e092
Kamil Kot <kamil.kot07@gmail.com>
2021-03-29 20:45:22 +0200

    added chat server

---
+++ src/AstormChatAPI/SocketsManager/ConnectionManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AstormChatAPI.SocketsManager
+{
+    public class ConnectionManager
+    {
+        private ConcurrentDictionary<string, WebSocket> _connections = new ConcurrentDictionary<string, WebSocket>();
+
+        public WebSocket GetSocketById(string id)
+        {
+            return _connections.FirstOrDefault(x => x.Key == id).Value;
+        }
+
+        public ConcurrentDictionary<string, WebSocket> GetAllConnections()
+        {
+            return _connections;
+        }
+
+        public string GetId(WebSocket socket)
+        {
+            return _connections.FirstOrDefault(x => x.Value == socket).Key;
+        }
+
+        public async Task RemoveSocketAsync(string id)
+        {
+            _connections.TryRemove(id, out var socket);
+            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "socket connection closed", CancellationToken.None);
+        }
+
+        public void AddSocket(WebSocket socket)
+        {
+            _connections.TryAdd(GetConnectionId(), socket);
+        }
+
+        private string GetConnectionId()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
---
+++ src/AstormChatAPI/SocketsManager/SocketsExtension.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace AstormChatAPI.SocketsManager
+{
+    public static class SocketsExtension
+    {
+        public static IServiceCollection AddWebSocketManager(this IServiceCollection services)
+        {
+            services.AddTransient<ConnectionManager>();
+            
+            foreach(var type in Assembly.GetEntryAssembly().ExportedTypes)
+            {
+                if (type.GetTypeInfo().BaseType == typeof(SocketHandler))
+                    services.AddSingleton(type);
+            }
+
+            return services;
+        }
+
+        public static IApplicationBuilder MapSockets(this IApplicationBuilder app, PathString path, SocketHandler socket)
+        {
+            return app.Map(path, (x) => x.UseMiddleware<SocketMiddelware>)
+        }
+    }
+}
