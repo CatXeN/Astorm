23f90aabab0023c5a92c8fcccb1cb8cde32e1d35
Szczepan Abramczyk <szczepanab@gmail.com>
2021-04-03 13:29:54 +0200

    menu and toolbar

--- src/AstormClient/src/app/app.module.ts
+++ src/AstormClient/src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { MainLayoutComponent } from './core/main-layout/main-layout.component';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToolbarComponent } from './core/toolbar/toolbar.component';
+import { MenuComponent } from './core/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainLayoutComponent
+    MainLayoutComponent,
+    ToolbarComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/AstormClient/src/app/shared/material.module.ts
+++ src/AstormClient/src/app/shared/material.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   imports: [
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports: [
-
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
