730a855c02aa6079e82c4b72da4bbbe9749088ad
Szczepan Abramczyk <szczepanab@gmail.com>
2021-04-03 22:21:06 +0200

    :D

--- src/AstormClient/src/app/core/core.module.ts
+++ src/AstormClient/src/app/core/core.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 
-
-
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     SharedModule,
-    
   ]
 })
 export class CoreModule { }
