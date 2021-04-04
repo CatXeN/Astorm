6815fd700a108797232fffe49f14225b829101f5
Igor Jedrzejek <igorjedrzejek@gmail.com>
2021-04-04 15:53:04 +0200

    Init database and organize sln

---
+++ domain/AstormDomain/Models/Entities/AssignUsersToServer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AstormDomain.Models.Entities
+{
+    public class AssignUsersToServer
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ServerId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        [ForeignKey("ServerId")]
+        public virtual Server Server { get; set; }
+    }
+}
---
+++ domain/AstormDomain/Models/Entities/Channel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
+
+namespace AstormDomain.Models.Entities
+{
+    public class Channel
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid ServerId { get; set; }
+        
+        [ForeignKey("ServerId")]
+        public virtual Server Server { get; set; }
+        public ICollection<ChannelMessage> ChannelMessages { get; } = new List<ChannelMessage>();
+    }
+}
