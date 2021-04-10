b95abd379627be6f051565b93fbbfe24432ff2e1
Kamil Kot <kamil.kot07@gmail.com>
2021-04-10 09:53:56 +0200

    style prototype

---
+++ src/Services/AstormChatServer/Services/Messages/IMessageService.cs
@@ -0,0 +1,13 @@
+﻿using AstormDomain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AstormChatServer.Services.Messages
+{
+    public interface IMessageService
+    {
+        Task AddMessage(Message message);
+    }
+}
