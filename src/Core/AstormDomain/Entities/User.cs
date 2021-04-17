04df075cabbba11a585ba8c307dd45ce6a2ba0c2
Kamil Kot <kamil.kot07@gmail.com>
2021-04-18 00:38:57 +0200

    fix

--- src/Core/AstormDomain/Entities/User.cs
+++ src/Core/AstormDomain/Entities/User.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace AstormDomain.Entities
 {
@@ -16,6 +17,7 @@ namespace AstormDomain.Entities
         public byte[] PasswordSalt { get; set; }
         public UserStatus UserStatus { get; set; }
 
+        [JsonIgnore]
         public ICollection<UserMessage> OwnerMessages { get; } = new List<UserMessage>();
         public ICollection<UserMessage> RecipientMessages { get; } = new List<UserMessage>();
         public ICollection<ChannelMessage> UserMessages { get; } = new List<ChannelMessage>();
